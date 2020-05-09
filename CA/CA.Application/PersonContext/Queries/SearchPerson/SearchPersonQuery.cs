f3c992e2f370b69a8a27139e8bc85c2dfa2778b6
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:13:04 +0800

    Added: SearchPersonQuery(Application)

---
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQuery.cs
@@ -0,0 +1,10 @@
+﻿using CA.Domain;
+using MediatR;
+using System.Collections.Generic;
+
+namespace CA.Application.PersonContext.Queries.SearchPerson
+{
+    public class SearchPersonQuery : IRequest<List<Person>>
+    {
+    }
+}
