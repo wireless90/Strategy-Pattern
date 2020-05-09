569694a12829c5c9c0b7969bd06e00b0458a5131
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:29:42 +0800

    Created: IPersonDbContext

---
+++ CA/CA.Application/Common/Interfaces/IPersonDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CA.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Common.Interfaces
+{
+    public interface IPersonDbContext
+    {
+        DbSet<Person> Persons { get; set; }
+    }
+}
