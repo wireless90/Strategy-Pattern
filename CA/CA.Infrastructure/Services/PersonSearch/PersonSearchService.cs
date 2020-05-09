41c7e58ef9516581e654c7c32eec14f1c8ba6d71
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:39:58 +0800

    Created PersonSearchService

---
+++ CA/CA.Infrastructure/Services/PersonSearch/PersonSearchService.cs
@@ -0,0 +1,24 @@
+﻿using CA.Application.Common.Interfaces;
+using CA.Domain;
+using System.Linq;
+
+namespace CA.Infrastructure.Services.PersonSearch
+{
+    public class PersonSearchService : IPersonSearchService
+    {
+        public IQueryable<Person> AsQueryable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPersonSearchService InitializeSet(IQueryable<Person> persons)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPersonSearchService SearchName(string nameOfStrategy, string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
