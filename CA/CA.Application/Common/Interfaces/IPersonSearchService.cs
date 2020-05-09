d3ee4f70e6d715b0bbd9c283434f555b9a88606e
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:39:24 +0800

    Amended: IPersonSearchService is now public

--- CA/CA.Application/Common/Interfaces/IPersonSearchService.cs
+++ CA/CA.Application/Common/Interfaces/IPersonSearchService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace CA.Application.Common.Interfaces
 {
-    interface IPersonSearchService
+    public interface IPersonSearchService
     {
         IPersonSearchService InitializeSet(IQueryable<Person> persons);
 
