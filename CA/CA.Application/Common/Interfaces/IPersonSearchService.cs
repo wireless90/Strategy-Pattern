dd9a08a9203011c5e51157a94a61c383598b1d51
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:29:55 +0800

    Created: IPersonSearchService

---
+++ CA/CA.Application/Common/Interfaces/IPersonSearchService.cs
@@ -0,0 +1,15 @@
+﻿using CA.Domain;
+using System;
+using System.Linq;
+
+namespace CA.Application.Common.Interfaces
+{
+    interface IPersonSearchService
+    {
+        IPersonSearchService InitializeSet(IQueryable<Person> persons);
+
+        IPersonSearchService SearchName(String nameOfStrategy);
+
+        IQueryable<Person> AsQueryable();
+    }
+}
