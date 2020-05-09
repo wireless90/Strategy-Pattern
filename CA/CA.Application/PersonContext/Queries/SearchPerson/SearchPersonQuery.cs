5ba3f5e968b7cfbce72895fb161c65176349c398
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:23:41 +0800

    Added Usecase Comments

--- CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQuery.cs
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQuery.cs
@@ -5,6 +5,18 @@ using System.Collections.Generic;
 
 namespace CA.Application.PersonContext.Queries.SearchPerson
 {
+    /// <summary>
+    ///     <Description>
+    ///         This query is used to search a Person given:
+    ///             1) Name
+    ///             2) IdentificationType and Identification
+    ///             3) Name and IdentificationType and Identification
+    ///             
+    ///         The Name search uses the PermutePlus Logic.
+    ///         The IdentificationType uses the Equals Logic.
+    ///         The Identification Uses the Contains Logic.
+    ///     </Description>
+    /// </summary>
     public class SearchPersonQuery : IRequest<List<Person>>
     {
         public String Name { get; set; }
