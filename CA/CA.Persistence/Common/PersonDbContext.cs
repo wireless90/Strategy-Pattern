ea7269e56ce0895912bace482e1c5718ef70f05c
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:33:12 +0800

    Creater PersonDbContext

---
+++ CA/CA.Persistence/Common/PersonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CA.Application.Common.Interfaces;
+using CA.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Persistence.Common
+{
+    public class PersonDbContext : DbContext, IPersonDbContext
+    {
+        public PersonDbContext()
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
