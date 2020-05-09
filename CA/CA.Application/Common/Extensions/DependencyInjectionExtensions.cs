496819c091d3ade4b376e368e1f6b40b7de7b52a
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:12:51 +0800

    Added: DependencyExtensions(Application) and registered Mediatr

---
+++ CA/CA.Application/Common/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CA.Application.Common.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddApplicationInjections(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
