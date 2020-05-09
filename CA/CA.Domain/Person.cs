5f4de225c1ea7b14798e8b0621bdcb252f3b4cbf
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:33:49 +0800

    Added Fields: PersonIdentification and PersonNames into Person

--- CA/CA.Domain/Person.cs
+++ CA/CA.Domain/Person.cs
@@ -1,7 +1,13 @@
-﻿namespace CA.Domain
+﻿using System.Collections.Generic;
+
+namespace CA.Domain
 {
     public class Person
     {
         public int Id { get; set; }
+
+        List<PersonIdentification> PersonIdentifications { get; set; }
+
+        List<PersonName> PersonNames { get; set; }
     }
 }
