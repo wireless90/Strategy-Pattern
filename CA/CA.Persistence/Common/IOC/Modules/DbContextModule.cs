213f606ae741b630c01435e1c0311a2cfe3a85eb
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:37:42 +0800

    Added IPersonDbContext Autofac registration

---
+++ CA/CA.Persistence/Common/IOC/Modules/DbContextModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using CA.Application.Common.Interfaces;
+
+namespace CA.Persistence.Common.IOC.Modules
+{
+    public class DbContextModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PersonDbContext>()
+                .As<IPersonDbContext>();
+
+            base.Load(builder);
+        }
+    }
+}
