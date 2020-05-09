901b75b3d3264bab4e451fc94d6bb467e23328be
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:30:10 +0800

    Created: SearchPersonQueryHandler

---
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using CA.Domain;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CA.Application.PersonContext.Queries.SearchPerson
+{
+    public class SearchPersonQueryHandler : IRequestHandler<SearchPersonQuery, List<Person>>
+    {
+        public SearchPersonQueryHandler()
+        {
+
+        }
+
+        public Task<List<Person>> Handle(SearchPersonQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
