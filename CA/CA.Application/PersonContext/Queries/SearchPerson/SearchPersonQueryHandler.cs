af83c45f396b4c5b5a8b0756b2521ea7eee12429
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:40:13 +0800

    Created: SearchPersonQueryHandler

--- CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQueryHandler.cs
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQueryHandler.cs
@@ -1,6 +1,10 @@
-﻿using CA.Domain;
+﻿using CA.Application.Common.Interfaces;
+using CA.Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,14 +12,31 @@ namespace CA.Application.PersonContext.Queries.SearchPerson
 {
     public class SearchPersonQueryHandler : IRequestHandler<SearchPersonQuery, List<Person>>
     {
-        public SearchPersonQueryHandler()
-        {
+        private readonly IPersonDbContext _personDbContext;
+        private readonly IPersonSearchService _personSearchService;
 
+        public SearchPersonQueryHandler(IPersonDbContext personDbContext, IPersonSearchService personSearchService)
+        {
+            _personDbContext = personDbContext;
+            _personSearchService = personSearchService;
         }
 
-        public Task<List<Person>> Handle(SearchPersonQuery request, CancellationToken cancellationToken)
+        public async Task<List<Person>> Handle(SearchPersonQuery request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Person> personsQuery = _personDbContext.Persons
+                .Include(person => person.PersonIdentifications)
+                .Where(person => 
+                    String.IsNullOrEmpty(request.Identification) ||
+                    String.IsNullOrEmpty(request.IdentificationType) ||
+                    person.PersonIdentifications.Any(p => 
+                        p.Identification.Equals(request.Identification) && p.Type.Equals(request.IdentificationType)
+                    )
+                );
+
+            return await _personSearchService.InitializeSet(personsQuery)
+                .SearchName("PermutePlus")
+                .AsQueryable()
+                .ToListAsync();
         }
     }
 }
