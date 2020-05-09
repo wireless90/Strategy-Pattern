a34147af813be678242cf17a0d380a0f71762fad
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:19:49 +0800

    Installed: Autofac.Extensions.DependencyInjection

--- CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQueryHandler.cs
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQueryHandler.cs
@@ -34,7 +34,7 @@ namespace CA.Application.PersonContext.Queries.SearchPerson
                 );
 
             return await _personSearchService.InitializeSet(personsQuery)
-                .SearchName("PermutePlus")
+                .SearchName("PermutePlus", request.Name)
                 .AsQueryable()
                 .ToListAsync();
         }
