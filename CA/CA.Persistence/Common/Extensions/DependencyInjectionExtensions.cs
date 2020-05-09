7c49ee529c4f93a87f2bfc57406e086e5c01f63a
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:21:58 +0800

    Added DependencyInjectionExtensions to Persistence

---
+++ CA/CA.Persistence/Common/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CA.Persistence.Common.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddPersistenceInjections(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
