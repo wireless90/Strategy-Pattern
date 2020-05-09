2f5abcf9598e89d45b8906def848d3741989d680
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:26:35 +0800

    Added: PersistenceModule

---
+++ CA/CA.Persistence/Common/IOC/PersistenceModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace CA.Persistence.Common.IOC
+{
+    public class PersistenceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
