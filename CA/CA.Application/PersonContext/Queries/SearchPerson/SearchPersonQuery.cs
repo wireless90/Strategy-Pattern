ed846a197a518fcaa6b490fcf0147d527c75ea56
Muhammad Razali <wireless-@hotmail.com>
2020-05-09 23:20:31 +0800

    Created: SearchPersonQuery(Application)

--- CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQuery.cs
+++ CA/CA.Application/PersonContext/Queries/SearchPerson/SearchPersonQuery.cs
@@ -1,10 +1,16 @@
 ﻿using CA.Domain;
 using MediatR;
+using System;
 using System.Collections.Generic;
 
 namespace CA.Application.PersonContext.Queries.SearchPerson
 {
     public class SearchPersonQuery : IRequest<List<Person>>
     {
+        public String Name { get; set; }
+
+        public String IdentificationType { get; set; }
+
+        public String Identification { get; set; }
     }
 }
