3e50553a8f9a2a409ee09cdc85e4b12d3df2a089
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:27:45 +0800

    Added: InfrastructureModule

---
+++ CA/CA.Infrastructure/Common/IOC/InfrastructureModule.cs
@@ -0,0 +1,13 @@
+﻿
+using Autofac;
+
+namespace CA.Infrastructure.Common.IOC
+{
+    public class InfrastructureModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
