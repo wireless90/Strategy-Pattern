c98266d0f41e81583719694aea10c6e3553190bc
Muhammad Razali <wireless-@hotmail.com>
2020-05-10 01:23:22 +0800

    Added DependencyInectionExtensions to Infrastructure

---
+++ CA/CA.Infrastructure/Common/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CA.Infrastructure.Common.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureInjections(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
